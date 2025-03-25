8fa7440623e37d60d791782a0c7e98492f0d083c
MaluCalland <malucalland@gmail.com>
2025-03-25 09:50:51 -0300

    aula picker

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Picker} from 'react-native-picker/picker'
+import {Picker} from '@react-native-picker/picker';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          pizza: 0, //so valor numerico
+          pizzas: [ //lista
+            {key: 1, nome: 'Portuguesa', valor: 25},
+            {key: 1, nome: 'Chocolate', valor: 28},
+            {key: 1, nome: 'Calabresa', valor: 20},
+            {key: 1, nome: 'Banana', valor: 22},
+            {key: 1, nome: 'Frango', valor: 24},
+            {key: 1, nome: 'Napolitana', valor: 26},
+          ]
+        };
+    }
+    render() {
+      let pizzasItem = this.state.pizzas.map( (v, k) => {
+        return <Picker.Item key={k} value={k} label={v.nome}/> //label: o que vai ta na linha - obrigatorio(mostrar na interface)
+      })
+
+      return(
+        //itemIndex : orinetação a objetos
+        <View style={styles.container}>
+          <Text style={styles.menu}></Text>
+          <Picker>
+            selectedValue = {this.state.pizza}
+            onValueChange = {(itemValue, itemIndex) => this.setState({pizza: itemValue})}
+            {pizzasItem}
+          </Picker>
+          <Text style={styles.pizzas}>Você escolheu o sabor:  
+            {this.state.pizzas[this.state.pizza].nome}
+          </Text>
+          <Text style={styles.pizzas}> Valor: R$
+            {this.state.pizzas[this.state.pizza].valor.toFixed(2)}
+          </Text>
+        </View>     
+      );
+    }
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
   },
+  menu:{
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  pizzas: {
+    marginTop: 20,
+    fontSize: 20,
+    textAlign: 'center'
+  }
 });
+
+export default App;
